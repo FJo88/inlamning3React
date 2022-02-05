19ec5936ee2314ef1ef336fb428fa8179b52de19
Filip Johnsson <filip.j.datatech@gmail.com>
2022-02-05 17:18:49 +0100

    Loader

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
+
 import {Link} from "react-router-dom"
 
 const HomePage = (props) => {
+    console.log(props)
     
     return ( 
         <div>
-            <h1>React Forum-inlämningsuppgift3</h1>
-            
-            {props.posts.map((post, userId, id) =>{
+            <h1>React Forum-inlämningsuppgift 3</h1>
+
+                {props.posts.map(({title, userId}, id) =>{
                 return <Link to=
-                {{pathname: `/post/${userId+1}`,}}><h2 key={id}>{post.title}</h2></Link>
+                {{pathname: `/post/${userId}`,}}><h2 key={id}>{title}</h2></Link>
             })}
         </div>
      );
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,9 +1,10 @@
 import {Link} from "react-router-dom"
 import { useParams } from "react-router-dom";
 import {useState, useEffect} from "react"
+import { CircularProgress } from "@material-ui/core";
 
 const PostPage = () => {
-    const[comments,setComments] = useState([]);
+    const[comments,setComments] = useState();
 
     const { id } = useParams();
     
@@ -11,12 +12,14 @@ const PostPage = () => {
         fetch(` https://jsonplaceholder.typicode.com/comments?postId=${id}`)
           .then((response) => response.json())
           .then((json) => setComments(json));
+         
       }, []);
       console.log(comments);
     return (  
         <div>
             <h1>Comments on Post {id}</h1>
-            {comments.map((comment, id) =>{
+            {comments ? (
+            comments.map((comment, id) =>{
                 return (
                 <div key={id} className="postinfo">
                     <p>{comment.body}</p>
@@ -26,7 +29,12 @@ const PostPage = () => {
                 )
                 
                    
-            })}
+            })) :(
+            <div>
+            <CircularProgress size={150}/>
+            </div>
+            )}
+            
             <Link to="/"><button className="home">Take me home</button></Link>
         </div>
     );
